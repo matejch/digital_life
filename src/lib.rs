da5bce1d5a5357552b8a466fbc8c5f4b16e62ef3
Matej Leban <matej@5insights.com>
2024-03-05 09:51:04 +0100

    add boilerplate code

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+mod utils;
+mod constants;
+mod particles;
+mod geometry;
+
+use wasm_bindgen::prelude::*;
+use web_sys::{console, js_sys};
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+        let cells = (0..width * height)
+            .map(|_i| {
+                if js_sys::Math::random() < 0.5 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+    pub fn tick(&mut self) {
+        console::log_1(&format!("yo yo").into());
+    }
+}
+
+
